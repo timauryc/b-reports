90ad03c1827187e96cef3d027b92289d8f52d4a9
Carlos Eduardo Timaury Moreno <MINUTRADEBH\carlos.timaury@mt0032-ubuntu.minutradebh.local>
2016-07-19 14:01:04 -0300

    first

---
+++ test/unit/summary-test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+suite('Bonuz Report Api validation tests ', function() {
+	test('#some test of something', function(done) {
+		console.log('testing something');
+		done();
+	});
+});
